542d21d17b7d53c7fd84147d7503fcdb0b499e8d
Jacob Hilton <jakehilton@jacobs-mbp.lan>
2020-11-15 19:29:03 -0700

    added windows for charachters

--- Develop/script.js
+++ Develop/script.js
@@ -6,11 +6,54 @@ var lowerArray = lower.split("");
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var upperArray = upper.split("");
 var special = "!@#$%^&*()-=+_~?";
-var specialArray = sepecial.split("");
+var specialArray = special.split("");
 var number = "0123456789";
 var numberArray = number.split("");
+
+
+
 // Generate password function
+function generatePassword() {
+  var allCharacters = [];
+  var confirmLength = window.prompt("Please chose a length of 8-128 characters: ");
+
+  while(confirmLength < 8 || confirmLength > 128) {
+    window.alert("You did not choose a password length bewtween 8-128 characters. Please try again.")
+    var confirmLength = window.prompt("Please chose a length of 8-128 characters: ");
+  }
+
+  while(allCharacters.length === 0) {
+    if(window.confirm("Would you like lowercase letters in your password?")) {
+      Array.prototype.push.apply(allCharacters, lowerArray)
+    }
+  
+    if(window.confirm("Would you like uppercase letters in your password?")) {
+      Array.prototype.push.apply(allCharacters, upperArray)
+    }
+  
+    if(window.confirm("Would you like special characters  in your password?")) {
+      Array.prototype.push.apply(allCharacters, specialArray)
+    }
+  
+    if(window.confirm("Would you like numbers in your password?")) {
+      Array.prototype.push.apply(allCharacters, numberArray)
+    }
+
+    if(allCharacters.length === 0){
+      window.alert("You need to select at least 1 type of character to generate the password.")
+    }
+
+    for (i = 0; i < confirmLength; )
+  }
+
+
+
+  
+}
+
 // char option variables
+
+
 // running while loop to check if all char option variables are false. If so, select again
 // while loop to check for length
 // if char option is selected, array name is stored in group array
@@ -19,7 +62,7 @@ var numberArray = number.split("");
 
 
 
-console.log(lowerArray)
+
 
 
 
